d148d15c4b6bc3ecad21aba566b4ef9e189aad68
Brandon Todd <121136424+Sysadmin7@users.noreply.github.com>
2023-01-27 10:41:53 -0500

    Find Software WMI Query
    Find Software WMI Query

---
+++ FindInstalledSoftware.sql
@@ -0,0 +1,1 @@
+SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceID = SMS_R_System.ResourceId where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%NAME%"
