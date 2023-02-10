992b70e181476a456fe5bf0b040a89ba0a1d2004
Brandon Todd <121136424+Sysadmin7@users.noreply.github.com>
2023-02-10 10:32:29 -0500

    Update RebootCronjobV3.sh

--- RebootCronjobV3.sh
+++ RebootCronjobV3.sh
@@ -8,12 +8,12 @@ MAXIMUM_SLEEP_SECONDS=$(($MAXIMUM_DELAY_HOURS * 3600))
 UPTIME=`/usr/bin/printf %.0f $(/usr/bin/awk '{print $0/3600;}' /proc/uptime)`
 SLEEP_TIME=$((1 + RANDOM % $MAXIMUM_SLEEP_SECONDS))
 /bin/sleep $SLEEP_TIME
-if /usr/bin/pgrep -x wfica > /dev/null; then
+if ! /usr/bin/pgrep -x wfica > /dev/null; then
   if [ $UPTIME -ge $REBOOT_HOURS ]; then
-    echo "${name} v${ver} : running /sbin/reboot
+    echo "${name} v${ver} : running /sbin/reboot"
     # /sbin/reboot
   else
-    echo "${name} v${ver}: running /sbin/get_rmsettings
+    echo "${name} v${ver}: running /sbin/get_rmsettings"
     # /sbin/get_rmsettings
   fi
 fi
