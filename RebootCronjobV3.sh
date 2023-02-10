602dee92c8683d0830fd7b7ab7ba9668a82d2c73
Brandon Todd <121136424+Sysadmin7@users.noreply.github.com>
2023-02-10 10:13:40 -0500

    Update RebootCronjobV3.sh

--- RebootCronjobV3.sh
+++ RebootCronjobV3.sh
@@ -1,17 +1,19 @@
-#IGEL Kiosk Reboot Script v3
 #!/bin/bash
+#IGEL Kiosk Reboot Script
+name=IGEL_KioskReboot_Script
+ver="0.3"
 REBOOT_HOURS=720
 MAXIMUM_DELAY_HOURS=2
 MAXIMUM_SLEEP_SECONDS=$(($MAXIMUM_DELAY_HOURS * 3600))
 UPTIME=`/usr/bin/printf %.0f $(/usr/bin/awk '{print $0/3600;}' /proc/uptime)`
 SLEEP_TIME=$((1 + RANDOM % $MAXIMUM_SLEEP_SECONDS))
 /bin/sleep $SLEEP_TIME
-if ! /usr/bin/pgrep -f wfica > /dev/null; then #Check that Citrix Workspace is NOT running.
-    if [ $UPTIME -ge $REBOOT_HOURS ]; then
-        /sbin/reboot
-    else
-        /sbin/get_rmsettings
-    fi
-fi 
-
-#if ! /usr/bin/pgrep -f wfica > /dev/null; then echo "Found wfica"; else "Did not find wfica"; fi
+if /usr/bin/pgrep -x wfica > /dev/null; then
+  if [ $UPTIME -ge $REBOOT_HOURS ]; then
+    echo "${name} v${ver} : running /sbin/reboot
+    # /sbin/reboot
+  else
+    echo "${name} v${ver}: running /sbin/get_rmsettings
+    # /sbin/get_rmsettings
+  fi
+fi
