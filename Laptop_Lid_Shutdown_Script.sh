7f1591a715bc23ad9118d85bf9833d74dde49e3e
Brandon Todd <121136424+Sysadmin7@users.noreply.github.com>
2023-04-17 14:21:48 -0400

    Update Laptop_Lid_Shutdown_Script.sh

--- Laptop_Lid_Shutdown_Script.sh
+++ Laptop_Lid_Shutdown_Script.sh
@@ -3,15 +3,16 @@
 # Brandon - btoddr22@outlook.com
 NAME=Laptop_Lid_Shutdown_Script
 VERSION="1.0"
+STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state" | sed -e 's/state://ig');
+
 echo "Lid Closed, Starting timer."
 sleep 30m #Sleep the script for 30 minutes.
 echo "Lid timer reached"
+
 grep -q close /proc/acpi/button/lid/*/state #Check if the laptop lid is closed.
-if [ $? = 0 ]; then
+if [ $? = 0 ] && [ $STATE == 'discharging' ]; then #Check condition of battery status.
     systemctl poweroff #Issue the shutdown command.
-fi
-grep -q open /proc/acpi/button/lid/*/state #Check if the laptop lid is open.
-if [ $? = 0 ]; then
+    else 
     echo "Lid is open. Not shutting down."
     exit; #Exit the script and do nothing.
 fi
