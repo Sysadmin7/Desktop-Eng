45b26d8fb19babe00bbe4e88051e6b6ec98695d0
Brandon Todd <121136424+Sysadmin7@users.noreply.github.com>
2023-04-17 10:15:12 -0400

    Shutdown IGEL Laptop if the lid has been closed for 30 minutes.
     Shutdown IGEL Laptop if the lid has been closed for 30 minutes.

---
+++ Laptop_Lid_Shutdown_Script_v2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# IGEL Laptop Lid Shutdown Script
+# Brandon - btoddr22@outlook.com
+NAME=Laptop_Lid_Shutdown_Script
+VERSION="1.0"
+STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state")
+
+echo "Lid Closed, Starting timer."
+sleep 30m #Sleep the script for 30 minutes.
+echo "Lid timer reached"
+
+grep -q close /proc/acpi/button/lid/*/state #Check if the laptop lid is closed.
+if [ $? = 0 ] && [ "$STATE" != 'discharging' ]; then #Check condition of battery status.
+    systemctl poweroff #Issue the shutdown command.
+grep -q open /proc/acpi/button/lid/*/state #Check if the laptop lid is open.
+elif [ $? = 0 ]; then
+    echo "Lid is open. Not shutting down."
+    exit; #Exit the script and do nothing.
+fi
