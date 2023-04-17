7752cc29a90e7d74c5a2a5ef9ed04967cc44fcb0
Brandon Todd <121136424+Sysadmin7@users.noreply.github.com>
2023-04-17 12:24:31 -0400

    Test battery status
    Test battery status

--- testbattery.sh
+++ testbattery.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 # Test returning battery state.
 
-STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state")
-if [ "$STATE" != 'discharging' ]; then
-        echo "Laptop not on AC Power"
+STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state" | sed -e 's/state://ig');
+if [ $STATE != 'discharging' ] then
+        echo "Laptop is on battery"
+else echo "Laptop is on AC Power"
 fi
